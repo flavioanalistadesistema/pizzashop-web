ca17983798f3720a032ad73cf6a105ea7213ec51
Flavio Santos <flavioanalistadesistema@gmail.com>
2024-03-26 21:41:39 -0300

    eslint não funciona

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import './global.css'
+import "./global.css";
 
-import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { RouterProvider } from 'react-router-dom'
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
 
-import { router } from './routes'
+import { Toaster } from "sonner";
+
+import { router } from "./routes";
 
 export function App() {
   return (
     <HelmetProvider>
+      <Toaster richColors />
       <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
     </HelmetProvider>
-  )
+  );
 }
